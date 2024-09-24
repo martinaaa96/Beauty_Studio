01f50d8890c33606700265b420602cb017337c86
Martina Vangelova <mvangelovaa@abv.bg>
2024-09-24 17:06:06 +0300

    update header

---
+++ beauty_studio/src/components/Header.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Header(){
+
+
+    return (
+    <>
+    <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 font-serif">
+<div className="m-7 md:my:0 md:ml-8 cursor: pointer">
+<img src="./logo.png" alt="logo" 
+className=""
+style={{ width: "150px" }}
+/>
+
+</div>
+
+
+    </div>
+    
+    </>
+    )
+}
