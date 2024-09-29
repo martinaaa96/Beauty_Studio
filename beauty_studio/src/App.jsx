2e85d890f195bf2e5d8bf0cb701fbebd518075bd
Martina Vangelova <mvangelovaa@abv.bg>
2024-09-29 20:05:05 +0300

    Create Login component

--- beauty_studio/src/App.jsx
+++ beauty_studio/src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Error from "./components/Error";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Error />
+      <Login />
       <Footer />
     </>
   );
---
+++ beauty_studio/src/components/Login.jsx
@@ -0,0 +1,56 @@
+export default function Login() {
+  return (
+    <div className="bg-no-repeat bg-cover bg-center relative bg-[url('/proba1.jpg')]">
+      <div className="absolute bg-gradient-to-b from-gray-200 to-red-200 opacity-75 inset-0 z-0"></div>
+      <div className="min-h-screen sm:flex sm:flex-row mx-0 justify-center">
+        <div className="flex-col flex  self-center p-10 sm:max-w-5xl xl:max-w-2xl  z-10">
+          <div className="self-start hidden lg:flex flex-col  text-white">
+            <img src="" className="mb-3" />
+            <h1 className="mb-3 font-bold text-5xl">Hello, beauty ! </h1>
+            <p className="pr-3">
+              If you have account please sing in and make your reservation
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center self-center  z-10">
+          <div className="p-12 bg-white mx-auto rounded-2xl w-100 ">
+            <div className="mb-4">
+              <h3 className="font-semibold text-2xl text-gray-800">Sign In </h3>
+              <p className="text-gray-500">Please sign in to your account.</p>
+            </div>
+            <div className="space-y-5">
+              <div className="space-y-2">
+                <label className="text-sm font-medium text-gray-700 tracking-wide">
+                  Email
+                </label>
+                <input
+                  className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-red-400"
+                  type=""
+                  placeholder="mail@gmail.com"
+                />
+              </div>
+              <div className="space-y-2">
+                <label className="mb-5 text-sm font-medium text-gray-700 tracking-wide">
+                  Password
+                </label>
+                <input
+                  className="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-red-400"
+                  type=""
+                  placeholder="Enter your password"
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center bg-red-400  hover:bg-red-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
+                >
+                  Sign in
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
