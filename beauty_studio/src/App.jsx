f8b97d19b22cc3d905ad287fbf5a792eb6293aa3
Martina Vangelova <mvangelovaa@abv.bg>
2024-09-29 14:45:22 +0300

    update App and add component

--- beauty_studio/src/App.jsx
+++ beauty_studio/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-//import Footer from "./components/Footer";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
