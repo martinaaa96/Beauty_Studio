24cf58e03e4315640060eaded175a6b50d41d9d9
Martina Vangelova <mvangelovaa@abv.bg>
2024-09-30 17:51:49 +0300

    Create Register component with tailwind css

--- beauty_studio/src/App.jsx
+++ beauty_studio/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Error from "./components/Error";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <Error />
       <Login />
+      <Register />
       <Footer />
     </>
   );
