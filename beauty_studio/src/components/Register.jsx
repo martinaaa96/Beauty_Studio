72ef22cc499ecc28bbdefb5ca8c588370b416ea4
Martina Vangelova <mvangelovaa@abv.bg>
2024-09-30 18:21:15 +0300

    update register form in sm-screen

--- beauty_studio/src/components/Register.jsx
+++ beauty_studio/src/components/Register.jsx
@@ -2,15 +2,14 @@ export default function Register() {
   return (
     <div className=" bg-contain bg-center relative bg-[url('/image1.jpg')]">
       <div className="absolute bg-gradient-to-b from-gray-200 to-orange-200 opacity-75 inset-0 z-0"></div>
-      <div className="min-h-screen sm:flex sm:flex-row mx-0 justify-center">
+      <div className="min-h-screen sm:flex sm:flex-row mx-0 justify-center relative z-10">
         <div className="flex-col flex  self-center p-10 sm:max-w-5xl xl:max-w-2xl  z-10">
           <div className="self-start hidden lg:flex flex-col  text-black">
-            <img src="" className="mb-3" />
             <h1 className="mb-3 font-bold text-5xl">Hello, beauty ! </h1>
             <p className="pr-3">If you dont have account - Make it from here</p>
           </div>
         </div>
-        <div className="flex justify-center self-center  z-10">
+        <div className="flex justify-center self-center z-10">
           <div className="p-12 bg-white mx-auto rounded-2xl w-100 ">
             <div className="mb-4">
               <h3 className="font-semibold text-2xl text-gray-800">
