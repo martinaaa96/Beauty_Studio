9c2f77c3dbd94451c4e987a93461fdc2a3b69822
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-01 12:21:23 +0300

    install framer-motion for more effects

--- beauty_studio/src/components/About.jsx
+++ beauty_studio/src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <section className="bg-white dark:bg-gray-900 font-serif">
       <div className="gap-16 items-center py-20 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-          <h2 className="mb-6 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          <h2 className="mb-6 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             WHO ARE WE
           </h2>
           <h3 className="text-black mb-6">
@@ -23,15 +23,11 @@ export default function About() {
           <p className="font-semibold">With us, beauty is a pleasure!</p>
         </div>
         <div className="grid grid-cols-2 gap-4 mt-8">
-          <img
-            className="w-full rounded-lg"
-            src={aboutpics}
-            alt="office content 1"
-          />
+          <img className="w-full rounded-lg" src={aboutpics} alt="content 1" />
           <img
             className="mt-4 w-full lg:mt-10 rounded-lg"
             src={aboutpics1}
-            alt="office content 2"
+            alt="content 2"
           />
         </div>
       </div>
