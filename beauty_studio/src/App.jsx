cc1c4fdf2f3505ca86bffbd134fa0b1878d0cd81
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-04 18:03:29 +0300

    update

--- beauty_studio/src/App.jsx
+++ beauty_studio/src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
-//import Home from "./components/Home";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Error from "./components/Error";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import About from "./components/About";
 import Services from "./components/Services";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <>
       <Header />
-     <Services/>
+      <Home />
+      <Gallery />
+      <Services />
       <About />
       <Error />
       <Login />
