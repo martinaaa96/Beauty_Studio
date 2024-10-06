c4abc65f0c3dd11abf9b24412caf31f64e208a9b
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-06 18:36:39 +0300

    fixed path from Error component

--- beauty_studio/src/components/Error.jsx
+++ beauty_studio/src/components/Error.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function Error() {
   return (
-    <div className="bg-red-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
-      <div className="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
+    <div className="bg-white w-full px-16 mt-4 md:px-0 h-screen flex items-center justify-center">
+      <div className="bg-white border border-black flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
         <p className="text-6xl md:text-7xl lg:text-9xl font-bold tracking-wider text-gray-300">
           404
         </p>
@@ -11,21 +13,23 @@ export default function Error() {
         <p className="text-gray-500 mt-4 pb-4 border-b-2 text-center">
           Sorry, the page you are looking for could not be found.
         </p>
-        <a
-          href="#"
-          className="flex items-center space-x-2 bg-red-200 hover:bg-amber-200 text-black px-4 py-2 mt-6 rounded transition duration-150"
-          title="Return Home"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+        <Link to="/">
+          <a
+            href="#"
+            className="flex items-center space-x-2 bg-red-200 hover:bg-amber-200 text-black px-4 py-2 mt-6 rounded transition duration-150"
+            title="Return Home"
           >
-            <path d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"></path>
-          </svg>
-          <span>Go to home page</span>
-        </a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"></path>
+            </svg>
+            <span>Go to home page</span>
+          </a>
+        </Link>
       </div>
     </div>
   );
--- beauty_studio/src/components/Services.jsx
+++ beauty_studio/src/components/Services.jsx
@@ -4,98 +4,96 @@ import aboutpics from "/aboutpics.jpg";
 
 export default function Services() {
   return (
-    
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-4xl font-bold text-center tracking-tight text-gray-900">
-          Our Services
-        </h2>
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
+      <h2 className="text-4xl font-bold text-center tracking-tight text-gray-900">
+        Our Services
+      </h2>
 
-        <div className="mt-24 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <div className="group relative border border-black rounded-lg p-4">
-            <div className="w-full overflow-hidden rounded-lg lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                alt="image"
-                src={aboutpics}
-                className="w-full h-auto object-cover max-h-48"
-              />
+      <div className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="group relative border border-black rounded-lg p-4">
+          <div className="w-full overflow-hidden rounded-lg lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt="image"
+              src={aboutpics}
+              className="w-full h-auto object-cover max-h-48"
+            />
 
-              <div className="mt-4 text-center">
-                <h3 className="text-md text-gray-900 font-midium">
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  Haircuts
-                </h3>
+            <div className="mt-4 text-center">
+              <h3 className="text-md text-gray-900 font-midium">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Haircuts
+              </h3>
 
-                <p className="mt-1 text-sm text-gray-700">
-                  Whether you prefer classic cuts or modern trends, we ensure a
-                  fresh and stylish result every time!
-                </p>
-              </div>
+              <p className="mt-1 text-sm text-gray-700">
+                Whether you prefer classic cuts or modern trends, we ensure a
+                fresh and stylish result every time!
+              </p>
             </div>
           </div>
-          <div className="group relative border border-black rounded-lg p-4">
-            <div className="w-full overflow-hidden rounded-md  lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                alt="image"
-                src={aboutpics}
-                className="w-full h-auto object-cover max-h-48"
-              />
+        </div>
+        <div className="group relative border border-black rounded-lg p-4">
+          <div className="w-full overflow-hidden rounded-md  lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt="image"
+              src={aboutpics}
+              className="w-full h-auto object-cover max-h-48"
+            />
 
-              <div className="mt-4 text-center">
-                <h3 className="text-md text-gray-900">
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  Styling
-                </h3>
-                <p className="mt-1 text-sm text-gray-700">
-                  From casual waves to elegant updos, we bring your vision to
-                  life with expert care!
-                </p>
-              </div>
+            <div className="mt-4 text-center">
+              <h3 className="text-md text-gray-900">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Styling
+              </h3>
+              <p className="mt-1 text-sm text-gray-700">
+                From casual waves to elegant updos, we bring your vision to life
+                with expert care!
+              </p>
             </div>
           </div>
-          <div className="group relative border border-black rounded-lg p-4">
-            <div className=" w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                alt="image"
-                src={aboutpics}
-                className="w-full h-auto object-cover max-h-48"
-              />
+        </div>
+        <div className="group relative border border-black rounded-lg p-4">
+          <div className=" w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt="image"
+              src={aboutpics}
+              className="w-full h-auto object-cover max-h-48"
+            />
 
-              <div className="mt-4 text-center">
-                <h3 className="text-md text-gray-900">
-                  <a>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    Coloring
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-700">
-                  Our coloring techniques enhance your natural beauty, creating
-                  bold or subtle changes that stand out!
-                </p>
-              </div>
+            <div className="mt-4 text-center">
+              <h3 className="text-md text-gray-900">
+                <a>
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  Coloring
+                </a>
+              </h3>
+              <p className="mt-1 text-sm text-gray-700">
+                Our coloring techniques enhance your natural beauty, creating
+                bold or subtle changes that stand out!
+              </p>
             </div>
           </div>
-          <div className="group relative border border-black rounded-lg p-4">
-            <div className=" w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                alt="image"
-                src={aboutpics}
-                className="w-full h-auto object-cover max-h-48"
-              />
+        </div>
+        <div className="group relative border border-black rounded-lg p-4">
+          <div className=" w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt="image"
+              src={aboutpics}
+              className="w-full h-auto object-cover max-h-48"
+            />
 
-              <div className="mt-4 text-center">
-                <h3 className="text-md text-gray-900">
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  Hair Extentions
-                </h3>
-                <p className="mt-1 text-sm text-gray-700">
-                  Enjoy fuller, longer hair that blends seamlessly with your
-                  natural look, boosting your confidence instantly!
-                </p>
-              </div>
+            <div className="mt-4 text-center">
+              <h3 className="text-md text-gray-900">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Hair Extentions
+              </h3>
+              <p className="mt-1 text-sm text-gray-700">
+                Enjoy fuller, longer hair that blends seamlessly with your
+                natural look, boosting your confidence instantly!
+              </p>
             </div>
           </div>
         </div>
       </div>
-    
+    </div>
   );
 }
