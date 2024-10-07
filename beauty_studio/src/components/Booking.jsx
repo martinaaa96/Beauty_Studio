8799b346a5df1b2e78aeb220c8644bab998b262d
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-07 16:59:25 +0300

    update booking

--- beauty_studio/src/components/Booking.jsx
+++ beauty_studio/src/components/Booking.jsx
@@ -1,3 +1,31 @@
 export default function Booking() {
-  return <></>;
+  return (
+    <>
+      <div className=" flex items-center justify-center p-10 mt-2 bg-white dark:bg-gray-900 font-serif ">
+        <ul className="divide-y divide-gray-100">
+          <li className="flex justify-between gap-x-6 py-5">
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                fgfdgdf
+              </p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                gdgsgzsdzg
+              </p>
+            </div>
+
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">fdsff</p>
+
+              <p className="mt-1 text-xs leading-5 text-gray-500">hfhfh</p>
+
+              <div className="mt-1 flex items-center gap-x-1.5">
+                
+             
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
