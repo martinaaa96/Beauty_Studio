8d657dcca02fe50c25e0d141e5964df2bb9025b7
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-08 15:47:16 +0300

    add more information

--- beauty_studio/src/components/Services.jsx
+++ beauty_studio/src/components/Services.jsx
@@ -93,6 +93,69 @@ export default function Services() {
             </div>
           </div>
         </div>
+        <div className="group relative border border-black rounded-lg p-4">
+          <div className="w-full overflow-hidden rounded-lg lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt="image"
+              src={aboutpics}
+              className="w-full h-auto object-cover max-h-48"
+            />
+
+            <div className="mt-4 text-center">
+              <h3 className="text-md text-gray-900 font-midium">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Manicure
+              </h3>
+
+              <p className="mt-1 text-sm text-gray-700">
+                Whether you prefer classic cuts or modern trends, we ensure a
+                fresh and stylish result every time!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group relative border border-black rounded-lg p-4">
+          <div className="w-full overflow-hidden rounded-lg lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt="image"
+              src={aboutpics}
+              className="w-full h-auto object-cover max-h-48"
+            />
+
+            <div className="mt-4 text-center">
+              <h3 className="text-md text-gray-900 font-midium">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Pedicure
+              </h3>
+
+              <p className="mt-1 text-sm text-gray-700">
+                Whether you prefer classic cuts or modern trends, we ensure a
+                fresh and stylish result every time!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group relative border border-black rounded-lg p-4">
+          <div className="w-full overflow-hidden rounded-lg lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt="image"
+              src={aboutpics}
+              className="w-full h-auto object-cover max-h-48"
+            />
+
+            <div className="mt-4 text-center">
+              <h3 className="text-md text-gray-900 font-midium">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Deep Cleasing
+              </h3>
+
+              <p className="mt-1 text-sm text-gray-700">
+                Whether you prefer classic cuts or modern trends, we ensure a
+                fresh and stylish result every time!
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
