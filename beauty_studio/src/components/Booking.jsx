f03c72d953086085360ae017cf10c64689f88936
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-09 15:48:38 +0300

    update

--- beauty_studio/src/components/Booking.jsx
+++ beauty_studio/src/components/Booking.jsx
@@ -1,9 +1,9 @@
 export default function Booking() {
   return (
     <>
-      <div className="flex flex-col sm:flex-row p-10 mt-4 bg-white dark:bg-gray-900 font-serif">
-<ul className="w-full">
-          <li className="flex flex-col sm:flex-row">
+      <div className="flex flex-col p-10 mt-4 border border-black rounded-lg bg-white dark:bg-gray-900 font-serif">
+        <ul className="w-full">
+          <li className="flex flex-col sm:flex-row items-center ">
             <div className="min-w-0 flex-auto">
               <h2 className="text-lg font-semibold leading-6 text-gray-900">
                 Pedicure
@@ -13,6 +13,7 @@ export default function Booking() {
                 your appointment!
               </p>
             </div>
+
             <div className="min-w-0 flex-auto">
               <h2 className="text-lg font-semibold leading-6 text-gray-900">
                 1h 30
@@ -21,15 +22,17 @@ export default function Booking() {
                 $50
               </p>
             </div>
+
             <div className=" shrink-0 sm:flex sm:flex-col sm:items-end mt-4 sm:mt-0">
               <button className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full">
                 Book
               </button>
             </div>
-            
           </li>
         </ul>
       </div>
+
+
     </>
   );
 }
