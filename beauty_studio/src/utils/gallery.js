c3ae7ca50382f2e7c4399612c82dcb2a9bd9a78f
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-11 13:08:31 +0300

    update gallery file

---
+++ beauty_studio/src/utils/gallery.js
@@ -0,0 +1,50 @@
+export const pictures = [
+    {
+        id: 1,
+        category: 'Haircuts',
+        image: '/blueberry.jpg',
+
+    },
+    {
+        id: 2,
+        category: 'Styling',
+        image: '/strawberry.jpg',
+
+
+
+    },
+    {
+        id: 3,
+        category: 'Coloring',
+        image: '/watermelon.jpg',
+
+
+    },
+    {
+        id: 4,
+        category: 'Hair Extentions',
+        image: '/lemonade.jpg',
+
+
+    },
+    {
+        id: 5,
+        category: 'Manicure',
+        image: '/blueberry.jpg',
+
+    },
+    {
+        id: 6,
+        category: 'Pedicure',
+        image: '/strawberry.jpg',
+
+
+
+    },
+    {
+        id: 7,
+        category: 'Deep Cleasing',
+        image: '/bananasmothie.jpg',
+
+    }
+]
