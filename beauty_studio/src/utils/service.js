0c01d8a19e1482a70b1907cebb35400acaf746d7
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-11 12:59:58 +0300

    create file with info for Service component

---
+++ beauty_studio/src/utils/service.js
@@ -0,0 +1,57 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Haircuts',
+        description: 'Whether you prefer classic cuts or modern trends, we ensure a fresh and stylish result every time!',
+        image: '/blueberry.jpg',
+
+    },
+    {
+        id: 2,
+        name: 'Styling',
+        description: 'From casual waves to elegant updos, we bring your vision to life with expert care!',
+        image: '/strawberry.jpg',
+
+
+
+    },
+    {
+        id: 3,
+        name: 'Coloring',
+        description: 'Our coloring techniques enhance your natural beauty, creating bold or subtle changes that stand out!',
+        image: '/watermelon.jpg',
+
+
+    },
+    {
+        id: 4,
+        name: 'Hair Extentions',
+        description: ' Enjoy fuller, longer hair that blends seamlessly with your natural look, boosting your confidence instantly!',
+        image: '/lemonade.jpg',
+
+
+    },
+    {
+        id: 5,
+        name: 'Manicure',
+        description: 'We offer a variety of colors and designs to give you the fresh,stylish look you deserve!',
+        image: '/blueberry.jpg',
+
+    },
+    {
+        id: 6,
+        name: 'Pedicure',
+        description: 'Relax and rejuvenate with our luxurious pedicure, designed to pamper your feet and nails!',
+        image: '/strawberry.jpg',
+
+
+
+    },
+    {
+        id: 7,
+        name: 'Deep Cleasing',
+        description: 'This intensive facial leaves your skin feeling purified, hydrated, and ready to face the day!',
+        image: '/bananasmothie.jpg',
+
+    }
+]
