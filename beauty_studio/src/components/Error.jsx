969c641e913266977a7d14ca1d7aa7e60da72448
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-12 17:57:59 +0300

    fixed Link paths

--- beauty_studio/src/components/Error.jsx
+++ beauty_studio/src/components/Error.jsx
@@ -13,22 +13,20 @@ export default function Error() {
         <p className="text-gray-500 mt-4 pb-4 border-b-2 text-center">
           Sorry, the page you are looking for could not be found.
         </p>
-        <Link to="/">
-          <a
-            href="#"
-            className="flex items-center space-x-2 bg-red-200 hover:bg-amber-200 text-black px-4 py-2 mt-6 rounded transition duration-150"
-            title="Return Home"
+        <Link
+          to="/"
+          className="flex items-center space-x-2 bg-red-200 hover:bg-amber-200 text-black px-4 py-2 mt-6 rounded transition duration-150"
+          title="Return Home"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"></path>
-            </svg>
-            <span>Go to home page</span>
-          </a>
+            <path d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"></path>
+          </svg>
+          <span>Go to home page</span>
         </Link>
       </div>
     </div>
