2217043d7a60ccd6d933e519dc5a1998e3460288
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-15 14:13:47 +0300

    update padding in table

--- beauty_studio/src/components/Booking.jsx
+++ beauty_studio/src/components/Booking.jsx
@@ -1,24 +1,107 @@
 export default function Booking() {
   return (
     <>
-
       <div className="flex min-h-screen items-center justify-center">
         <div className="overflow-x-auto">
           <table className="min-w-full bg-white shadow-md rounded-xl">
-            <thead>
-              <tr className="bg-blue-gray-100 text-gray-700">
-                <th className="py-3 px-4 text-center">Services</th>
-              </tr>
-            </thead>
             <tbody className="text-blue-gray-900">
               <tr className="border-b border-blue-gray-200">
-                <td className="py-3 px-4">Pedicure</td>
-                <td className="py-3 px-4">
+                <td className="py-6 px-4">Haircuts</td>
+                <td className="py-6 px-4">
                   Select your preferred time from our available slots and book
                   your appointment!
                 </td>
-
-                <td className="py-3 px-4">
+                <td className="py-6 px-4">
+                  <a
+                    href="#"
+                    className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
+                  >
+                    Book
+                  </a>
+                </td>
+              </tr>
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-6 px-4">Styling</td>
+                <td className="py-6 px-4">
+                  Select your preferred time from our available slots and book
+                  your appointment!
+                </td>
+                <td className="py-6 px-4">
+                  <a
+                    href="#"
+                    className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
+                  >
+                    Book
+                  </a>
+                </td>
+              </tr>
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-6 px-4">Coloring</td>
+                <td className="py-6 px-4">
+                  Select your preferred time from our available slots and book
+                  your appointment!
+                </td>
+                <td className="py-6 px-4">
+                  <a
+                    href="#"
+                    className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
+                  >
+                    Book
+                  </a>
+                </td>
+              </tr>
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-6 px-4">Hair Extentions</td>
+                <td className="py-6 px-4">
+                  Select your preferred time from our available slots and book
+                  your appointment!
+                </td>
+                <td className="py-6 px-4">
+                  <a
+                    href="#"
+                    className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
+                  >
+                    Book
+                  </a>
+                </td>
+              </tr>
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-6 px-4">Pedicure</td>
+                <td className="py-6 px-4">
+                  Select your preferred time from our available slots and book
+                  your appointment!
+                </td>
+                <td className="py-6 px-4">
+                  <a
+                    href="#"
+                    className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
+                  >
+                    Book
+                  </a>
+                </td>
+              </tr>
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-6 px-4">Manicure</td>
+                <td className="py-6 px-4">
+                  Select your preferred time from our available slots and book
+                  your appointment!
+                </td>
+                <td className="py-6 px-4">
+                  <a
+                    href="#"
+                    className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
+                  >
+                    Book
+                  </a>
+                </td>
+              </tr>
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-6 px-4">Deep Cleasing</td>
+                <td className="py-6 px-4 ">
+                  Select your preferred time from our available slots and book
+                  your appointment!
+                </td>
+                <td className="py-6 px-4">
                   <a
                     href="#"
                     className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
