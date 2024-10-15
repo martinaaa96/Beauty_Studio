08b8074fc84971a4eb10b6d7331c4c74c7ebd2a4
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-15 13:23:49 +0300

    update Booking component

--- beauty_studio/src/components/Booking.jsx
+++ beauty_studio/src/components/Booking.jsx
@@ -1,38 +1,36 @@
 export default function Booking() {
   return (
     <>
-      <div className="flex flex-col p-10 mt-4 border border-black rounded-lg bg-white dark:bg-gray-900 font-serif">
-        <ul className="w-full">
-          <li className="flex flex-col sm:flex-row items-center ">
-            <div className="min-w-0 flex-auto">
-              <h2 className="text-lg font-semibold leading-6 text-gray-900">
-                Pedicure
-              </h2>
-              <p className="mt-4 truncate text-md leading-5 text-gray-500">
-                Select your preferred time from our available slots and book
-                your appointment!
-              </p>
-            </div>
 
-            <div className="min-w-0 flex-auto">
-              <h2 className="text-lg font-semibold leading-6 text-gray-900">
-                1h 30
-              </h2>
-              <p className="mt-3 truncate text-md leading-5 text-gray-500">
-                $50
-              </p>
-            </div>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white shadow-md rounded-xl">
+            <thead>
+              <tr className="bg-blue-gray-100 text-gray-700">
+                <th className="py-3 px-4 text-center">Services</th>
+              </tr>
+            </thead>
+            <tbody className="text-blue-gray-900">
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-3 px-4">Pedicure</td>
+                <td className="py-3 px-4">
+                  Select your preferred time from our available slots and book
+                  your appointment!
+                </td>
 
-            <div className=" shrink-0 sm:flex sm:flex-col sm:items-end mt-4 sm:mt-0">
-              <button className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full">
-                Book
-              </button>
-            </div>
-          </li>
-        </ul>
+                <td className="py-3 px-4">
+                  <a
+                    href="#"
+                    className="text-black bg-red-300 hover:bg-amber-200 py-2 px-4 rounded-full"
+                  >
+                    Book
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-
-
     </>
   );
 }
