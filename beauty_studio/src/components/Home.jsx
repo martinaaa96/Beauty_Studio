8e8d114ab05c3dca278cd88ff6a173cec35ca6f3
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-18 16:34:23 +0300

    add buttons for carousel

--- beauty_studio/src/components/Home.jsx
+++ beauty_studio/src/components/Home.jsx
@@ -1,17 +1,23 @@
 //import home from "../assets/home.jpg";
 //import home2 from "../assets/home2.jpg";
 //import home3 from "../assets/home3.jpg";
-import proba from "../assets/proba.jpg";
-import proba4 from "../assets/proba4.jpg";
+//import proba from "../assets/proba.jpg";
+//import proba4 from "../assets/proba4.jpg";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-between items-center text-right p-4">
-        <div className="w:690px h-[720px] md:h-[600px] sm:h-[400px] xs:h-[300px]">
-          <div className="w:690px  h-auto ">
-            <img src={proba} className="w-full h-full object-cover" />
-          </div>
+      <div className="h-screen bg-indigo-500 grid place-content-center">
+        <div className="flex items-center gap-2">
+          <button className="min-w-[30px] h-[30px] rounded-full grid place-content-center text-white bg-black bg-opacity-20 hover:bg-opacity-60 duration-200">
+            <MdKeyboardArrowLeft />
+          </button>
+          <div className="max-w-[300px] grid place-items-center rounded-lg h-[200px] bg-white"></div>
+          <button className="min-w-[30px] h-[30px] rounded-full grid place-content-center text-white bg-black bg-opacity-20 hover:bg-opacity-60 duration-200">
+            <MdKeyboardArrowRight />
+          </button>
         </div>
       </div>
     </>
