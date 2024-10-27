4fa72f801a2973173c342dbd122eaa100975839c
Martina Vangelova <mvangelovaa@abv.bg>
2024-10-27 16:57:35 +0200

    change home pic

--- beauty_studio/src/components/Home.jsx
+++ beauty_studio/src/components/Home.jsx
@@ -1,14 +1,14 @@
 //import home from "../assets/home.jpg";
 //import home2 from "../assets/home2.jpg";
 //import home3 from "../assets/home3.jpg";
-//import proba from "../assets/proba.jpg";
-import proba4 from "../assets/proba4.jpg";
+import proba from "../assets/proba.jpg";
+//import proba4 from "../assets/proba4.jpg";
 
 export default function Home() {
   return (
     <>
       <div>
-        <img src={proba4} />
+        <img src={proba} />
       </div>
     </>
   );
