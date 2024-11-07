adbfb0403a776c59ff136340e45b3aaabf69b227
Martina Vangelova <mvangelovaa@abv.bg>
2024-11-07 15:41:52 +0200

    respon screen and buttons

--- beauty_studio/src/components/Home.jsx
+++ beauty_studio/src/components/Home.jsx
@@ -32,30 +32,30 @@ export default function Home() {
         transition={{ type: "spring", stiffness: 300, damping: 30 }}
       >
         {images.map((src, index) => (
-          <div key={index} className="w-full flex-shrink-0 h-full">
+          <div key={index} className="w-full flex-shrink-0 h-full relative">
             <img
               src={src}
               alt={`Image ${index + 1}`}
               className="w-full h-full object-cover rounded-lg shadow-lg"
             />
+
+            <div className="absolute inset-0 flex justify-between items-center px-4">
+              <button
+                onClick={handlePrev}
+                className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white rounded-full p-2 opacity-75 hover:opacity-100 w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12"
+              >
+                <FaArrowLeft />
+              </button>
+              <button
+                onClick={handleNext}
+                className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white rounded-full p-2 opacity-75 hover:opacity-100 w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12"
+              >
+                <FaArrowRight />
+              </button>
+            </div>
           </div>
         ))}
       </motion.div>
-
-      <div className="absolute inset-0 flex justify-between items-center px-4">
-        <button
-          onClick={handlePrev}
-          className="bg-gray-700 text-white rounded-full p-2 opacity-75 hover:opacity-100"
-        >
-          <FaArrowLeft />
-        </button>
-        <button
-          onClick={handleNext}
-          className="bg-gray-700 text-white rounded-full p-2 opacity-75 hover:opacity-100"
-        >
-          <FaArrowRight />
-        </button>
-      </div>
     </div>
   );
 }
